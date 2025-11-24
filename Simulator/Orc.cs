9833e5dd793805d9d3e2f67ea60ad29e79899a20
DawidBozek98 <dawid.bozek98@gmail.com>
2025-11-24 17:53:06 +0100

    Elfy i orki - poprawa

--- Simulator/Orc.cs
+++ Simulator/Orc.cs
@@ -1,20 +1,43 @@
-﻿using Simulator;
-using System.Xml.Linq;
+﻿namespace Simulator;
 
 public class Orc : Creature
 {
-    public int Rage { get; set; } = 1;
-    public void Hunt() => Console.WriteLine($"{Name} is hunting.");
+    private int _rage;
+    private int _huntCount = 0;
 
-    public Orc(string name, int level, int rage) : base(name, level)
+    public int Rage
+    {
+        get => _rage;
+        init
+        {
+            int v = value;
+            if (v < 0) v = 0;
+            if (v > 10) v = 10;
+            _rage = v;
+        }
+    }
+
+    public Orc() { }
+
+    public Orc(string name, int level = 1, int rage = 0)
+        : base(name, level)
     {
         Rage = rage;
     }
 
-
-
     public override void SayHi()
     {
-        Console.WriteLine($"Hi! I'm {Name} (level {Level}, rage {Rage}.");
+        Console.WriteLine($"{Name} the Orc (Lvl {Level}, Rage {Rage})");
     }
+
+    public void Hunt()
+    {
+        _huntCount++;
+        Console.WriteLine($"{Name} hunts...");
+
+        if (_huntCount % 2 == 0 && _rage < 10)
+            _rage++;
+    }
+
+    public override int Power => 7 * Level + 3 * Rage;
 }
