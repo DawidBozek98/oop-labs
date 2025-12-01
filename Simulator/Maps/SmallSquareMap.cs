87d47f5994148b0b58202522ff6879922edfaa87
DawidBozek98 <dawid.bozek98@gmail.com>
2025-12-01 19:14:22 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,38 @@
+﻿namespace Simulator.Maps;
+
+public class SmallSquareMap : Map
+{
+    private readonly Rectangle _bounds;
+
+    public int Size { get; }
+
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size > 20)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(size),
+                "Size must be between 5 and 20."
+            );
+        }
+
+        Size = size;
+
+        _bounds = new Rectangle(0, 0, Size - 1, Size - 1);
+    }
+
+    public override bool Exist(Point p) =>
+        _bounds.Contains(p);
+
+    public override Point Next(Point p, Direction d)
+    {
+        var next = p.Next(d);
+        return Exist(next) ? next : p;   // zatrzymujemy ruch
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        var next = p.NextDiagonal(d);
+        return Exist(next) ? next : p;   // zatrzymujemy ruch
+    }
+}
---
+++ Simulator/Point.cs
@@ -0,0 +1,35 @@
+﻿namespace Simulator;
+
+public readonly struct Point
+{
+    public readonly int X, Y;
+
+    public Point(int x, int y) => (X, Y) = (x, y);
+
+    public override string ToString() => $"({X}, {Y})";
+
+    public Point Next(Direction direction) =>
+        direction switch
+        {
+            Direction.Up => new Point(X, Y + 1),
+            Direction.Right => new Point(X + 1, Y),
+            Direction.Down => new Point(X, Y - 1),
+            Direction.Left => new Point(X - 1, Y),
+            _ => this // ciekawostka, deafult świeci się na czerwono i podpowiada to
+        };
+
+    // rotujemy o 45 stopni w prawo
+    public Point NextDiagonal(Direction direction) =>
+        direction switch
+        {
+            // Up - Up-Right
+            Direction.Up => new Point(X + 1, Y + 1),
+            // Right - Down-Right
+            Direction.Right => new Point(X + 1, Y - 1),
+            // Down - Down-Left
+            Direction.Down => new Point(X - 1, Y - 1),
+            // Left - Up-Left
+            Direction.Left => new Point(X - 1, Y + 1),
+            _ => this
+        };
+}
